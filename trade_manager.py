18efe69814338e746a282fdca47382d2f92c72f7
Fanatic1989 <christiansucre1@gmail.com>
2026-03-15 12:45:07 -0400

    Update trade_manager.py

--- trade_manager.py
+++ trade_manager.py
@@ -8,11 +8,13 @@ def calculate_trade():
 
     global balance
 
-    trade = balance * (config.RISK_PERCENT/100)
+    risk = config.RISK_PERCENT / 100
 
-    return round(trade,2)
+    size = balance * risk
 
-def place_trade(market,direction):
+    return round(size,2)
+
+def place_trade(market, direction):
 
     global balance
 
@@ -20,17 +22,17 @@ def place_trade(market,direction):
 
     result = random.choice(["W","L","D"])
 
-    if result=="W":
-        balance += size*0.8
+    if result == "W":
+        balance += size * 0.8
 
-    elif result=="L":
+    elif result == "L":
         balance -= size
 
     trade = {
-        "market":market,
-        "direction":direction,
-        "size":size,
-        "result":result
+        "market": market,
+        "direction": direction,
+        "size": size,
+        "result": result
     }
 
     save_trade(trade)
@@ -39,11 +41,12 @@ def save_trade(trade):
 
     try:
         with open("trade_history.json") as f:
-            data=json.load(f)
+            data = json.load(f)
+
     except:
-        data=[]
+        data = []
 
     data.append(trade)
 
     with open("trade_history.json","w") as f:
-        json.dump(data,f)
+        json.dump(data,f,indent=2)
